d46deaea39d71493b624de9184ed18a4ff64f332
unknown <onerofad>
2025-11-09 17:53:44 +0000

    first commit

--- src/components/mobile/About/HomeMobileAbout.js
+++ src/components/mobile/About/HomeMobileAbout.js
@@ -1,6 +1,10 @@
 import NavBarMobile from "../NavbarMobile"
 import FooterMobile from '../FooterMobile' 
 import WallPaperAboutMobile from "./WallPaperAboutMobile"
+import MissionAboutMobile from "./MissionAbotMobile"
+import PartnersMobile from "./PartnersMobile"
+import BloodDonationAboutMobile from "./VloodDonationAboutMobile"
+import TransparencyMobile from "./TransparencyMobile"
 
 
 const HomeMobileAbout = () => {
@@ -8,7 +12,10 @@ const HomeMobileAbout = () => {
         <>
             <NavBarMobile />
             <WallPaperAboutMobile />
-         
+            <MissionAboutMobile />
+            <PartnersMobile />
+            <BloodDonationAboutMobile />
+            <TransparencyMobile />
             <FooterMobile />
         
         </>
