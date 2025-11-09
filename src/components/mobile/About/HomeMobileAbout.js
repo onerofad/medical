4e87741369478c35e340356180ab569ab84d907c
unknown <onerofad>
2025-11-09 17:01:54 +0000

    first commit

--- src/components/mobile/About/HomeMobileAbout.js
+++ src/components/mobile/About/HomeMobileAbout.js
@@ -1,21 +1,12 @@
 import NavBarMobile from "../NavbarMobile"
-{/*import FooterMobile from '../FooterMobile'
-import WallPaperAboutMobile from "./WallPaperAboutMobile"
-import MissionAboutMobile from "./MissionAbotMobile"
-import PartnersMobile from "./PartnersMobile"
-import BloodDonationAboutMobile from "./VloodDonationAboutMobile"
-import TransparencyMobile from "./TransparencyMobile"*/}
+
 
 const HomeMobileAbout = () => {
     return(
         <>
             <NavBarMobile />
-            {/*<WallPaperAboutMobile />
-            <MissionAboutMobile />
-            <PartnersMobile />
-            <BloodDonationAboutMobile />
-            <TransparencyMobile />
-            <FooterMobile />*/}
+         
+            <FooterMobile />
         
         </>
     )
