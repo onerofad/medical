528fb6d06cc57af051fa6003a9ba8e7b0e350b2e
unknown <onerofad>
2025-12-01 16:23:28 +0100

    first commit

--- src/components/mobile/donor/HomeMobileDonor.js
+++ src/components/mobile/donor/HomeMobileDonor.js
@@ -11,7 +11,6 @@ const HomeMobileDonor = () => {
         <BloodCompatibilityMobile />
         <DonationCenterMobile />
         <FindDonationMobile />
-        <FooterMobile />
         </>
 
     )
