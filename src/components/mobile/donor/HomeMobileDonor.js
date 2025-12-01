74cbbbae45760e2e465299195dc2fff93a4704eb
unknown <onerofad>
2025-12-01 16:32:52 +0100

    first commit

--- src/components/mobile/donor/HomeMobileDonor.js
+++ src/components/mobile/donor/HomeMobileDonor.js
@@ -1,16 +1,10 @@
-import FooterMobile from "../FooterMobile"
 import NavBarMobile from "../NavbarMobile"
-import BloodCompatibilityMobile from "./BloodCompatibilityMobile"
-import DonationCenterMobile from "./DonationCenterMobile"
-import FindDonationMobile from "./FindDonationMobile"
 
 const HomeMobileDonor = () => {
     return(
         <>
         <NavBarMobile />
-        <BloodCompatibilityMobile />
-        <DonationCenterMobile />
-        <FindDonationMobile />
+    
         </>
 
     )
