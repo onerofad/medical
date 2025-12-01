15cbe1465460be0df9a4a765323716939c60c0f7
unknown <onerofad>
2025-12-01 16:16:45 +0100

    first commit

--- src/components/mobile/donor/FindDonationMobile.js
+++ src/components/mobile/donor/FindDonationMobile.js
@@ -7,15 +7,15 @@ import '../../../css/style.css'
 const FindDonationMobile = () => {
     return(
         <>
-            <div style={{margin: 0, padding: '50px 100px', background: '#F8F9FA'}}>
+            <div style={{margin: 0, padding: '50px 0px', background: '#F8F9FA'}}>
                 <Header
-                style={{
+                 style={{
                     color: '#333',
                     textAlign: 'center',
                     leadingTrim: 'both',
                     textEdge: 'cap',
                     fontFamily: 'Inter',
-                    fontSize: '32px',
+                    fontSize: '24px',
                     fontStyle: 'normal',
                     fontWeight: 700,
                     lineHeight: 'normal'
@@ -28,11 +28,11 @@ const FindDonationMobile = () => {
                     color: '#333',
                     textAlign: 'center',
                     fontFamily: 'Inter',
-                    fontSize: '20px',
+                    fontSize: '14px',
                     fontStyle: 'normal',
                     fontWeight: 400,
                     lineHeight: '30px',
-                    padding: '0px 240px'
+                    padding: '0px 20px'
                 }}
             >
                 Locate the nearest blood donation center with just a few clicks. Our easy-to-use map and search tool help you schedule an appointment and join the lifesaving mission today.
