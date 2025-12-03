8ad0537e0d800f3aef3753792f7c9cb0e03b09de
frank <frank@gmail.com>
2025-12-03 02:29:56 -0800

    first commit

--- src/components/mobile/donor/FindDonationMobile.js
+++ src/components/mobile/donor/FindDonationMobile.js
@@ -1,4 +1,4 @@
-import { Grid, Header, Image, Search } from "semantic-ui-react"
+import { Grid, GridColumn, Header, Image, Search } from "semantic-ui-react"
 import mapImage from '../../../assets/images/map.png'
 
 import '../../../css/style.css'
@@ -31,17 +31,17 @@ const FindDonationMobile = () => {
                     fontStyle: 'normal',
                     fontWeight: 400,
                     lineHeight: '30px',
-                    padding: '0px 20px'
+                    padding: '0px auto'
                 }}
             >
                 Locate the nearest blood donation center with just a few clicks. Our easy-to-use map and search tool help you schedule an appointment and join the lifesaving mission today.
             </Header>
-            <div style={{margin: 0, padding: '0px 0px', background: '#F8F9FA'}}>
-                <Grid textAlign="center" style={{}}>
+            <Grid columns={1} style={{margin: 0, padding: '0px 0px', background: '#F8F9FA'}}>
+                <GridColumn textAlign="center">
                     <Search size="big" placeholder="Use Location" />
-                    <Image size="large" style={{marginTop: 30}} src={mapImage} />
-                </Grid>
-            </div>
+                    <Image fluid style={{marginTop: 30}} src={mapImage} />
+                </GridColumn>
+            </Grid>
         </div>     
     )
 }
