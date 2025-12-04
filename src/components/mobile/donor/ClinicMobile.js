47c91637957d35905527d9c3810256d778cfb9c5
frank <frank@gmail.com>
2025-12-04 13:44:31 -0800

    first commit

--- src/components/mobile/donor/ClinicMobile.js
+++ src/components/mobile/donor/ClinicMobile.js
@@ -6,8 +6,8 @@ const Clinics = () => {
         <div style={{margin: 0, padding: '50px 0px', background: '#FFF'}}>
             <Header id="big_text"  content="Gifted Hand Clinic" />
             <Divider style={{backgroundColor: '#D10000'}} />
-            <Grid>
-                <GridColumn width={6}>
+            <p>
+                <p>
                     <Header id="big_text2" content="ADDRESS" />
                     <p id="small_text2">
                         3 Atere Junction, Off Omole Phase 2, Ikosi Isheri, Lagos
@@ -18,8 +18,8 @@ const Clinics = () => {
                     <p id="small_text2">a
                         08148353163
                     </p>
-                </GridColumn>
-                <GridColumn width={2}>
+                </p>
+                <p>
                     <Header id="big_text2" content="DAYS" />
                     <p id="small_text2">
                         Monday
@@ -39,8 +39,8 @@ const Clinics = () => {
                     <p id="small_text2">
                         Saturday
                     </p>
-                </GridColumn>
-                <GridColumn width={4}>
+                </p>
+                <p>
                     <Header id="big_text2" content="HOURS" />
                     <p id="small_text2">
                         7:30 AM - 5:30 AM
@@ -60,8 +60,8 @@ const Clinics = () => {
                     <p id="small_text2">
                         7:30 AM - 5:00 AM
                     </p>
-                </GridColumn>
-                <GridColumn width={4}>
+                </p>
+                <p>
                     <p id="big_text3" style={{marginTop: 40}}>
                         <Icon size="large" name="calendar outline" color="red" />
                          COLLECTION TYPE
@@ -74,159 +74,9 @@ const Clinics = () => {
                         <Icon size="large" name="calendar outline" color="red" />
                         GET DIRECTION
                     </p>
-                </GridColumn>
+                </p>
 
-            </Grid>
-
-
-            <Header id="big_text"  content="Free Land  Clinic" />
-            <Divider style={{backgroundColor: '#D10000'}} />
-            <Grid>
-                <GridColumn width={6}>
-                    <Header id="big_text2" content="ADDRESS" />
-                    <p id="small_text2">
-                        No 56 olowara street ojudu berger 
-                    </p>
-                    <p id="small_text2">
-                        phone number
-                    </p>
-                    <p id="small_text2">
-                        08147686589
-                    </p>
-                </GridColumn>
-                <GridColumn width={2}>
-                    <Header id="big_text2" content="DAYS" />
-                    <p id="small_text2">
-                        Monday
-                    </p>
-                    <p id="small_text2">
-                        Tuesday
-                    </p>
-                    <p id="small_text2">
-                        Wednesday
-                    </p>
-                    <p id="small_text2">
-                        Thursday
-                    </p>
-                    <p id="small_text2">
-                        Friday
-                    </p>
-                    <p id="small_text2">
-                        Saturday
-                    </p>
-                </GridColumn>
-                <GridColumn width={4}>
-                    <Header id="big_text2" content="HOURS" />
-                    <p id="small_text2">
-                        8:30 AM - 5:30 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 6:00 AM
-                    </p>
-                    <p id="small_text2">
-                        8:30 AM - 4:00 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 4:00 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 5:00 AM
-                    </p>
-                    <p id="small_text2">
-                        8:30 AM - 5:00 AM
-                    </p>
-                </GridColumn>
-                <GridColumn width={4}>
-                    <p id="big_text3" style={{marginTop: 40}}>
-                        <Icon size="large" name="calendar outline" color="red" />
-                         COLLECTION TYPE
-                    </p>
-                    <p id="big_text3">
-                        <Icon size="large" name="calendar outline" color="red" />
-                        SCHEDULE APPOINTMENT
-                    </p>
-                    <p id="big_text3">
-                        <Icon size="large" name="calendar outline" color="red" />
-                        GET DIRECTION
-                    </p>
-                </GridColumn>
-
-            </Grid>
-
-
-            <Header id="big_text"  content="Valley Oak Hospital" />
-            <Divider style={{backgroundColor: '#D10000'}} />
-            <Grid>
-                <GridColumn width={6}>
-                    <Header id="big_text2" content="ADDRESS" />
-                    <p id="small_text2">
-                        Address: 10 Uduma Kalu Street, River Valley Estate, Ojodu Berger, Lagos
-                    </p>
-                    <p id="small_text2">
-                        phone number
-                    </p>
-                    <p id="small_text2">
-                        07038253560
-                    </p>
-                </GridColumn>
-                <GridColumn width={2}>
-                    <Header id="big_text2" content="DAYS" />
-                    <p id="small_text2">
-                        Monday
-                    </p>
-                    <p id="small_text2">
-                        Tuesday
-                    </p>
-                    <p id="small_text2">
-                        Wednesday
-                    </p>
-                    <p id="small_text2">
-                        Thursday
-                    </p>
-                    <p id="small_text2">
-                        Friday
-                    </p>
-                    <p id="small_text2">
-                        Saturday
-                    </p>
-                </GridColumn>
-                <GridColumn width={4}>
-                    <Header id="big_text2" content="HOURS" />
-                    <p id="small_text2">
-                        8:00 AM - 5:30 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 6:00 AM
-                    </p>
-                    <p id="small_text2">
-                        8:00 AM - 4:00 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 4:00 AM
-                    </p>
-                    <p id="small_text2">
-                        7:30 AM - 5:00 AM
-                    </p>
-                    <p id="small_text2">
-                        8:00 AM - 5:00 AM
-                    </p>
-                </GridColumn>
-                <GridColumn width={4}>
-                    <p id="big_text3" style={{marginTop: 40}}>
-                        <Icon size="large" name="calendar outline" color="red" />
-                         COLLECTION TYPE
-                    </p>
-                    <p id="big_text3">
-                        <Icon size="large" name="calendar outline" color="red" />
-                        SCHEDULE APPOINTMENT
-                    </p>
-                    <p id="big_text3">
-                        <Icon size="large" name="calendar outline" color="red" />
-                        GET DIRECTION
-                    </p>
-                </GridColumn>
-
-            </Grid>
+            </p>
             
         </div>
     )
