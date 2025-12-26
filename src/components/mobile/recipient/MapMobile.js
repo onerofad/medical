9a6b6309afcb735ce0677da3be1c04f2b9fdf9df
frank <frank@gmail.com>
2025-12-26 14:31:00 -0800

    first commit

--- src/components/mobile/recipient/MapMobile.js
+++ src/components/mobile/recipient/MapMobile.js
@@ -104,10 +104,10 @@ const MapMobile = () => {
                         </FormField>
                     </Form>
 
-                </GridColumn>*/}
+                </GridColumn>
                 <GridColumn style={{padding: 0}}>
                     <Image src={map} centered style={{}} />
-                </GridColumn>
+                </GridColumn>*/}
             </Grid>
         </div>
     )
